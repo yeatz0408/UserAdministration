f066d1777a105f0f2771148b9dd27af901bba350
unknown <yeatz0408@gmail.com>
2023-04-02 07:58:07 +0900

    Start backend

---
+++ UserManagementSystemBackend/src/main/java/com/gmail/yeatz0408/UserManagementSystemBackend/UserManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.yeatz0408.UserManagementSystemBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementSystemBackendApplication.class, args);
+	}
+
+}
---
+++ UserManagementSystemBackend/src/test/java/com/gmail/yeatz0408/UserManagementSystemBackend/UserManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.yeatz0408.UserManagementSystemBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
