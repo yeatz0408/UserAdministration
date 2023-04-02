f3a53afc2e5473d2b253b7b6764adc0d0d4744c2
unknown <yeatz0408@gmail.com>
2023-04-03 06:47:29 +0900

    replace getters and setters with Lombok

--- UserAdministrationBackend/src/main/java/com/gmail/yeatz0408/UserAdministrationBackend/model/User.java
+++ UserAdministrationBackend/src/main/java/com/gmail/yeatz0408/UserAdministrationBackend/model/User.java
@@ -3,9 +3,13 @@ package com.gmail.yeatz0408.UserAdministrationBackend.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @NoArgsConstructor
 public class User {
 
@@ -16,31 +20,4 @@ public class User {
     private String name;
     private String email;
     
-    public Long getId() {
-        return id;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public String getName() {
-        return name;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    
-    
-
 }
