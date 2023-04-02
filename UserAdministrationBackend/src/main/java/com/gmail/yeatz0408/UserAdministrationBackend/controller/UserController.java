55006f2ba0baf25aa674a991715da4a67ae444e0
unknown <yeatz0408@gmail.com>
2023-04-03 06:15:11 +0900

    add GET mapping

--- UserAdministrationBackend/src/main/java/com/gmail/yeatz0408/UserAdministrationBackend/controller/UserController.java
+++ UserAdministrationBackend/src/main/java/com/gmail/yeatz0408/UserAdministrationBackend/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.gmail.yeatz0408.UserAdministrationBackend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +20,10 @@ public class UserController {
     @PostMapping("/user")
     User newUser(@RequestBody User newUser) {
         return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
