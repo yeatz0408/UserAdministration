882065212330767e003ab1d94daf7c6675dde350
unknown <yeatz0408@gmail.com>
2023-04-04 19:37:03 +0900

    create AddUser page and add routes

--- user-administration-frontend/src/App.js
+++ user-administration-frontend/src/App.js
@@ -2,12 +2,23 @@ import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './layout/Navbar';
 import Home from './pages/Home'
+import AddUser from './users/AddUser'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/adduser" element={<AddUser/>} />
+        </Routes>
+      </Router>
+
+      
+      
     </div>
   );
 }
