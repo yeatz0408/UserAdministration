8617a7eb34f0b505454ce44fa81de76dbd16190e
unknown <yeatz0408@gmail.com>
2023-04-05 05:55:31 +0900

    add submit and cancel function for AddUser

--- user-administration-frontend/src/layout/Navbar.js
+++ user-administration-frontend/src/layout/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">User Administration</Link>
+                    <Link className="navbar-brand" to="/">アカウント管理システム</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
--- user-administration-frontend/src/users/AddUser.js
+++ user-administration-frontend/src/users/AddUser.js
@@ -1,47 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 export default function AddUser() {
-  return (
-    <div className="container">
-        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
-            <h2 className="text-center m-4">アカウント登録</h2>
-            <div className="mb-3">
-                <label htmlFor="Name" className="form-label">
-                    名前
-                </label>
-                <input 
-                    type={"text"}
-                    className="form-control"
-                    placeholder="名前を入力してください。"
-                    name="name"
-                />
-            </div>
-            <div className="mb-3">
-                <label htmlFor="username" className="form-label">
-                    アカウントID
-                </label>
-                <input 
-                    type={"text"}
-                    className="form-control"
-                    placeholder="アカウントIDを入力してください。"
-                    name="username"
-                />
-            </div>
-            <div className="mb-3">
-                <label htmlFor="email" className="form-label">
-                    Eメール
-                </label>
-                <input 
-                    type={"text"}
-                    className="form-control"
-                    placeholder="Eメールを入力してください。"
-                    name="email"
-                />
-            </div>
-            <button type="submit" className="btn btn-outline-primary px-4">登録</button>
-            <button type="submit" className="btn btn-danger mx-2 px-1">取り消し</button>
-        </div>
 
-    </div>
-  )
+    let navigate = useNavigate();
+
+    const [user, setUser] = useState({
+        name: "",
+        username: "",
+        email: ""
+    })
+
+    const { name, username, email } = user;
+
+    const onInputChange = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value });
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        await axios.post("http://localhost:8080/user", user);
+        navigate("/");
+    }
+
+    return (
+        <div className="container">
+            <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+                <h2 className="text-center m-4">アカウント登録</h2>
+                <form onSubmit={(e) => onSubmit(e)}>
+                    <div className="mb-3">
+                        <label htmlFor="Name" className="form-label">
+                            名前
+                        </label>
+                        <input
+                            type={"text"}
+                            className="form-control"
+                            placeholder="名前を入力してください。"
+                            name="name"
+                            value={name}
+                            onChange={(e) => onInputChange(e)}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="username" className="form-label">
+                            アカウントID
+                        </label>
+                        <input
+                            type={"text"}
+                            className="form-control"
+                            placeholder="アカウントIDを入力してください。"
+                            name="username"
+                            value={username}
+                            onChange={(e) => onInputChange(e)}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">
+                            Eメール
+                        </label>
+                        <input
+                            type={"text"}
+                            className="form-control"
+                            placeholder="Eメールを入力してください。"
+                            name="email"
+                            value={email}
+                            onChange={(e) => onInputChange(e)}
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-outline-primary px-4">登録</button>
+                    <Link to="/" className="btn btn-danger mx-2 px-1" >取り消し</Link>
+                </form>
+            </div>
+
+        </div>
+    )
 }
