f31bea2ca3510eeb44d3da9ce73f244077433687
unknown <yeatz0408@gmail.com>
2023-04-05 15:47:45 +0900

    add PutMapping to backend

--- UserAdministrationBackend/src/main/java/com/gmail/yeatz0408/UserAdministrationBackend/controller/UserController.java
+++ UserAdministrationBackend/src/main/java/com/gmail/yeatz0408/UserAdministrationBackend/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,5 +36,17 @@ public class UserController {
     User getUserById(@PathVariable Long id) {
         return userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+
+                    return userRepository.save(user);
+                }).orElseThrow(() -> new UserNotFoundException(id));
     }
 }
