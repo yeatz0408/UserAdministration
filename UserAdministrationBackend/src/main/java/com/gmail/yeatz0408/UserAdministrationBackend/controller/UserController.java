e15b08b12a751d6ba595bf0d5983bb582786c380
unknown <yeatz0408@gmail.com>
2023-04-05 16:08:59 +0900

    add DeleteMapping to backend

--- UserAdministrationBackend/src/main/java/com/gmail/yeatz0408/UserAdministrationBackend/controller/UserController.java
+++ UserAdministrationBackend/src/main/java/com/gmail/yeatz0408/UserAdministrationBackend/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,5 +49,15 @@ public class UserController {
 
                     return userRepository.save(user);
                 }).orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id) {
+        if (!userRepository.existsById(id)) {
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+
+        return "User with id " +id+ " has been deleted successfully.";
     }
 }
